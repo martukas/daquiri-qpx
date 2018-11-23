b87c6d0293739f682cd1a3582b2a59f87d0f2c80
Martin Shetty <martukas@gmail.com>
2018-11-23 15:30:28 +0100

    "CHN and MCA parsers improved; updates #2"

--- source/importers/ImporterCHN.cpp
+++ source/importers/ImporterCHN.cpp
@@ -5,7 +5,7 @@
 
 #include <core/util/custom_logger.h>
 
-struct __attribute__ ((packed)) ChnHeader
+struct __attribute__ ((packed)) Header
 {
   int16_t must_be_neg1;
   int16_t detector;
@@ -32,49 +32,48 @@ void ImporterCHN::import(const boost::filesystem::path& path, DAQuiri::ProjectPt
 {
   std::ifstream file(path.string(), std::ios::binary);
 
-  ChnHeader header;
-  file.read(reinterpret_cast<char*>(&header), sizeof(ChnHeader));
+  Header header;
+  file.read(reinterpret_cast<char*>(&header), sizeof(Header));
 
   if (header.must_be_neg1 != -1)
     throw std::runtime_error("<ImporterCHN> invalid CHN file, marker != -1");
 
+  std::vector<int32_t> spectrum(static_cast<size_t>(header.num_channels));
+  file.read(reinterpret_cast<char*>(spectrum.data()), spectrum.size() * sizeof(int32_t));
+
+  INFO("Detector = {}", header.detector);
+  INFO("Segment = {}", header.segment);
+
+  std::stringstream stream;
   std::string start_day(&header.start_date[0], 2);
   std::string start_month(&header.start_date[2], 3);
   std::string start_year =
       ((header.start_date[7] == '1') ? "20" : "19")
           + std::string(&header.start_date[5], 2);
+  stream << start_year << "-" << start_month << "-" << start_day;
 
+  // \todo if time is invalid?
   std::string start_hours(&header.start_time[0], 2);
   std::string start_mins(&header.start_time[2], 2);
   std::string start_secs(&header.start_seconds[0], 2);
+  stream << " " << start_hours << ":" << start_mins << ":" << start_secs;
 
   date::sys_time<std::chrono::seconds> t;
-  std::stringstream stream;
-  stream << start_year << "-" << start_month << "-" << start_day
-         << " " << start_hours << ":" << start_mins << ":" << start_secs;
-
-  INFO("Detector = {}", header.detector);
-  INFO("Segment = {}", header.segment);
-
-  INFO("Start time = {}", stream.str());
   stream >> date::parse("%Y-%b-%d %H:%M:%S", t);
   if (stream.fail())
     throw std::runtime_error("<ImporterCHN> failed to parse date");
-  INFO("parsed start time = {}", date::format("%FT%TZ", t));
 
-  std::vector<int32_t> spectrum(static_cast<size_t>(header.num_channels));
-  file.read(reinterpret_cast<char*>(spectrum.data()), spectrum.size() * sizeof(int32_t));
+  auto lt_ms = static_cast<int64_t>(header.live_time) * 20;
+  auto rt_ms = static_cast<int64_t>(header.real_time) * 20;
 
   auto hist = DAQuiri::ConsumerFactory::singleton().create_type("Histogram 1D");
   if (!hist)
     throw std::runtime_error("<ImporterCHN> could not get a valid Histogram 1D from factory");
 
+  hist->set_attribute(DAQuiri::Setting::text("name", path.stem().string()));
+  hist->set_attribute(DAQuiri::Setting::boolean("visible", true));
   hist->set_attribute(DAQuiri::Setting("start_time", t));
-
-  auto lt_ms = static_cast<int64_t>(header.live_time) * 20;
   hist->set_attribute(DAQuiri::Setting("live_time", std::chrono::milliseconds(lt_ms)));
-
-  auto rt_ms = static_cast<int64_t>(header.real_time) * 20;
   hist->set_attribute(DAQuiri::Setting("real_time", std::chrono::milliseconds(rt_ms)));
 
   for (size_t i = 0; i < spectrum.size(); ++i)
@@ -85,9 +84,6 @@ void ImporterCHN::import(const boost::filesystem::path& path, DAQuiri::ProjectPt
     new_entry.second = PreciseFloat(spectrum[i]);
     entry_list.push_back(new_entry);
   }
-
-  hist->set_attribute(DAQuiri::Setting::text("name", path.stem().string()));
-  hist->set_attribute(DAQuiri::Setting::boolean("visible", true));
 
   hist->import(*this);
 
--- source/importers/ImporterMCA.cpp
+++ source/importers/ImporterMCA.cpp
@@ -5,7 +5,7 @@
 
 #include <core/util/custom_logger.h>
 
-struct __attribute__ ((packed)) TimeB
+struct __attribute__ ((packed)) Time
 {
   int32_t time;
   int16_t milli_time;
@@ -16,12 +16,12 @@ struct __attribute__ ((packed)) TimeB
 struct __attribute__ ((packed)) Elapsed
 {
   int32_t live_time;
-  int32_t true_time;
+  int32_t real_time;
   int32_t sweeps;
   double comp;
 };
 
-struct __attribute__ ((packed)) XCal
+struct __attribute__ ((packed)) Calibration
 {
   float ecal2;
   float ecal1;
@@ -32,19 +32,19 @@ struct __attribute__ ((packed)) XCal
   char corder;
 };
 
-struct __attribute__ ((packed)) McaHeader16k
+struct __attribute__ ((packed)) Header
 {
   int16_t read_type;
   int16_t mca_number;
   int16_t read_region;
-  int32_t tagno;
+  int32_t tag;
   char spectrum_name[26];
   int16_t acquisition_mode;
-  TimeB acquisition_start;
+  Time start_time;
   Elapsed elapsed;
-  XCal xcal;
+  Calibration calibration;
   int16_t filler[19]; // \todo why is this higher than in specs?
-  int16_t nchans;
+  int16_t num_channels;
 };
 
 bool ImporterMCA::validate(const boost::filesystem::path& path) const
@@ -56,46 +56,50 @@ void ImporterMCA::import(const boost::filesystem::path& path, DAQuiri::ProjectPt
 {
   std::ifstream file (path.string(), std::ios::binary);
 
-  std::vector<char> buf(sizeof(McaHeader16k));
+  Header header;
+  file.read(reinterpret_cast<char*>(&header), sizeof(Header));
+  std::vector<int32_t> spectrum(static_cast<size_t>(header.num_channels));
+  file.read(reinterpret_cast<char*>(spectrum.data()), spectrum.size() * sizeof(int32_t));
 
-  INFO("Read array size: {}", buf.size());
-  file.read(buf.data(), buf.size());
+  std::string spec_name(&header.spectrum_name[0], 26);
+  INFO("Name: {}", spec_name);
+  INFO("Mode: {}", header.acquisition_mode);
+  INFO("Read type: {}", header.read_type);
+  INFO("Read region: {}", header.read_region);
+  INFO("MCA number: {}", header.mca_number);
+  INFO("Tag: {}", header.tag);
 
-  McaHeader16k *header = reinterpret_cast<McaHeader16k *>(buf.data());
+  date::sys_time<std::chrono::seconds> t;
+  t += std::chrono::seconds(header.start_time.time);
+  INFO("time.ms_time: {}", header.start_time.milli_time);
+  INFO("time.time_zone: {}", header.start_time.time_zone);
+  INFO("time.DST_flag: {}", header.start_time.DST_flag);
 
-  INFO("Name: {}", std::string(header->spectrum_name));
-  INFO("Mode: {}", header->acquisition_mode);
+  auto lt_ms = static_cast<int64_t>(header.elapsed.live_time) * 10;
+  auto rt_ms = static_cast<int64_t>(header.elapsed.real_time) * 10;
+  INFO("elapsed.sweeps: {}", header.elapsed.sweeps);
+  INFO("elapsed.comp: {}", header.elapsed.comp);
 
-  INFO("XCal.units: {}", std::string(header->xcal.unit_name));
-  INFO("XCal.ecal0: {}", header->xcal.ecal0);
-  INFO("XCal.ecal1: {}", header->xcal.ecal1);
-  INFO("XCal.ecal2: {}", header->xcal.ecal2);
-
-  INFO("time.time: {}", header->acquisition_start.time);
-  INFO("time.ms_time: {}", header->acquisition_start.milli_time);
-  INFO("time.time_zone: {}", header->acquisition_start.time_zone);
-
-  INFO("elapsed.live: {}", header->elapsed.live_time);
-  INFO("elapsed.true: {}", header->elapsed.true_time);
-
-
-  INFO("Nchans: {}", header->nchans);
-
-  // \todo why is this signed?
-  // \todo should value of nchans be used?
-  std::vector<int32_t> spectrum(16384);
-  file.read(reinterpret_cast<char*>(spectrum.data()), spectrum.size()*4);
+  std::string cal_units(&header.calibration.unit_name[0], 5);
+  std::string cal_unit_type(&header.calibration.unit_type, 1);
+  std::string cal_cformat(&header.calibration.cformat, 1);
+  std::string cal_corder(&header.calibration.corder, 1);
+  INFO("XCal.ecal0: {}", header.calibration.ecal0);
+  INFO("XCal.ecal1: {}", header.calibration.ecal1);
+  INFO("XCal.ecal2: {}", header.calibration.ecal2);
+  INFO("XCal.units: {}", cal_units);
+  INFO("XCal.unit_type: {}", cal_unit_type);
+  INFO("XCal.cformat: {}", cal_cformat);
+  INFO("XCal.corder: {}", cal_corder);
 
   auto hist = DAQuiri::ConsumerFactory::singleton().create_type("Histogram 1D");
   if (!hist)
-    throw std::runtime_error("ImporterMCA could not get a valid Histogram 1D from factory");
+    throw std::runtime_error("<ImporterMCA> could not get a valid Histogram 1D from factory");
 
-  hist->set_attribute(DAQuiri::Setting::text("description", std::string(header->xcal.unit_name)));
-
-  auto lt_ms = static_cast<int64_t>(header->elapsed.live_time * 1000.0);
+  hist->set_attribute(DAQuiri::Setting::text("name", path.stem().string()));
+  hist->set_attribute(DAQuiri::Setting::boolean("visible", true));
+  hist->set_attribute(DAQuiri::Setting("start_time", t));
   hist->set_attribute(DAQuiri::Setting("live_time", std::chrono::milliseconds(lt_ms)));
-
-  auto rt_ms = static_cast<int64_t>(header->elapsed.true_time * 1000.0);
   hist->set_attribute(DAQuiri::Setting("real_time", std::chrono::milliseconds(rt_ms)));
 
   for (size_t i=0; i < spectrum.size(); ++i)
@@ -107,8 +111,6 @@ void ImporterMCA::import(const boost::filesystem::path& path, DAQuiri::ProjectPt
     entry_list.push_back(new_entry);
   }
 
-  hist->set_attribute(DAQuiri::Setting::text("name", path.stem().string()));
-  hist->set_attribute(DAQuiri::Setting::boolean("visible", true));
 
   hist->import(*this);
 
