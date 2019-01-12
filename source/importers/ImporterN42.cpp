37819c7afe024975b29c21da83785798876868e1
Martin Shetty <martukas@gmail.com>
2019-01-11 23:00:50 -0600

    "N42 calibration improting and tests; updates #2"

--- source/importers/ImporterN42.cpp
+++ source/importers/ImporterN42.cpp
@@ -3,6 +3,7 @@
 #include <core/consumer_factory.h>
 #include <importers/string_to_chans.h>
 #include <pugixml.hpp>
+#include <core/util/string_extensions.h>
 
 #include <core/util/custom_logger.h>
 
@@ -37,25 +38,28 @@ void ImporterN42::import(const boost::filesystem::path& path, DAQuiri::ProjectPt
   std::string data;
 
   data = node.child_value("RealTime");
-  if (!data.empty()) {
+  if (!data.empty())
+  {
     boost::algorithm::trim(data);
     if (data.size() > 3)
-      data = data.substr(2, data.size()-3); //to trim PTnnnS to nnn
+      data = data.substr(2, data.size() - 3); //to trim PTnnnS to nnn
     auto rt_ms = static_cast<int64_t>(std::stod(data) * 1000.0);
     hist->set_attribute(DAQuiri::Setting("real_time", std::chrono::milliseconds(rt_ms)));
   }
 
   data = node.child_value("LiveTime");
-  if (!data.empty()) {
+  if (!data.empty())
+  {
     boost::algorithm::trim(data);
     if (data.size() > 3)
-      data = data.substr(2, data.size()-3); //to trim PTnnnS to nnn
+      data = data.substr(2, data.size() - 3); //to trim PTnnnS to nnn
     auto lt_ms = static_cast<int64_t>(std::stod(data) * 1000.0);
     hist->set_attribute(DAQuiri::Setting("live_time", std::chrono::milliseconds(lt_ms)));
   }
 
   data = node.child_value("StartTime");
-  if (!data.empty()) {
+  if (!data.empty())
+  {
     hist->set_attribute(DAQuiri::Setting("start_time", from_iso_extended(data)));
   }
 
@@ -65,24 +69,50 @@ void ImporterN42::import(const boost::filesystem::path& path, DAQuiri::ProjectPt
 
   entry_list = string_to_chans_zero_suppressed(channel_data);
 
-  // \todo import calibrations
-//  Detector newdet;
-//  if (node.attribute("Detector"))
-//    newdet.set_name(node.attribute("Detector").value());
-//  if (node.child_value("DetectorType"))
-//    newdet.set_type(node.child_value("DetectorType"));
-//
-//  //have more claibrations? fwhm,etc..?
-//  if (node.child("Calibration"))
-//  {
-//    Calibration newcalib(bits_);
-//    newcalib.from_xml(node.child("Calibration"));
-//    newdet.set_energy_calibration(newcalib);
-//  }
-//
-//  metadata_.detectors.resize(1);
-//  metadata_.detectors[0] = newdet;
+  std::string detname = "unknown";
+  if (node.attribute("Detector"))
+    detname = node.attribute("Detector").value();
 
+  DAQuiri::Detector newdet(detname);
+  if (node.child_value("DetectorType"))
+    newdet.set_type(node.child_value("DetectorType"));
+
+  // \todo more calibrations - fwhm,etc..?
+  if (node.child("Calibration"))
+  {
+    DAQuiri::CalibID from("energy", detname, "");
+    DAQuiri::CalibID to("energy", detname, "keV");
+
+    auto calibnode = node.child("Calibration");
+
+//    if (calibnode.child_value("CalibrationCreationDate"))
+//      auto calib_date = from_iso_extended(calibnode.child_value("CalibrationCreationDate"));
+    if (calibnode.attribute("EnergyUnits"))
+      to.units = std::string(calibnode.attribute("EnergyUnits").value());
+
+    std::string model_str = std::string(calibnode.child("Equation").attribute("Model").value());
+    auto coefs = std::string(calibnode.child("Equation").child_value("Coefficients"));
+
+    std::stringstream ss(trim_copy(coefs));
+    std::vector<double> calibration;
+    double coef;
+    while (ss.rdbuf()->in_avail())
+    {
+      ss >> coef;
+      calibration.push_back(coef);
+    }
+
+    DAQuiri::Calibration new_calib(from, to);
+    new_calib.function(model_str, calibration);
+
+    newdet.set_calibration(new_calib);
+  }
+
+//  DBG("det = {}", newdet.debug(""));
+
+  hist->set_detectors({newdet});
+
+  hist->set_attribute(DAQuiri::Setting::text("value_latch", "energy"));
   hist->set_attribute(DAQuiri::Setting::text("name", path.stem().string()));
   hist->set_attribute(DAQuiri::Setting::boolean("visible", true));
 
