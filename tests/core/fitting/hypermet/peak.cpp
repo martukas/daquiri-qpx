95deef6f1aff4f6397f4b79db1976512b53bcc50
Martin Shetty <martukas@gmail.com>
2019-02-03 22:57:47 -0600

    "tests for value and peak; updates #6"

--- tests/core/fitting/hypermet/peak.cpp
+++ tests/core/fitting/hypermet/peak.cpp
@@ -1,33 +1,180 @@
 #include "gtest_color_print.h"
 
-#include <dlib/matrix/matrix_mat.h>
+#include "visualize_vector.h"
 
-#include <core/fitting/hypermet/Step.h>
+#include <core/fitting/hypermet/Peak.h>
 
-class Step : public TestBase
+class Peak : public TestBase
 {
-  DAQuiri::Step step;
+ protected:
 
   virtual void SetUp()
   {
-    step.amplitude.bound(0.000001, 0.05);
-    step.amplitude.to_fit = true;
+    peak = peak.gaussian_only();
+    peak.amplitude.bound(0, 500);
+    peak.amplitude.val(400);
+    peak.position.bound(5, 15);
+    peak.position.val(10);
+    peak.width_override = true;
+    peak.width_.val(3.2);
   }
 
-  double diff()
+  DAQuiri::WeightedData generate_data()
   {
-    DAQuiri::PrecalcVals pre;
-    pre.width = 2;
-    pre.ampl = 6;
-    pre.half_ampl = 3;
-    pre.spread = 10;
-//    const auto& fe = std::bind(&Region::eval, region_, std::placeholders::_1);
-//    double length(derivative(rosen)(starting_point) - rosen_derivative(starting_point));
+    std::vector<double> channels;
+    std::vector<double> y;
+    for (size_t i = 0; i < 20; ++i)
+    {
+      channels.push_back(i);
+      y.push_back(peak.eval(i).all());
+    }
+    return DAQuiri::WeightedData(channels, y);
   }
+
+  void grad_at_point(DAQuiri::Value& variable, size_t channel_idx)
+  {
+    auto wdata = generate_data();
+
+    int32_t idx {0};
+    peak.update_indices(idx);
+    EXPECT_EQ(idx, 3);
+
+    double degrees_freedom = wdata.data.size() - idx;
+    EXPECT_EQ(degrees_freedom, 17);
+
+    auto chosen_point = wdata.data.at(channel_idx);
+    EXPECT_EQ(chosen_point.x, channel_idx);
+    EXPECT_EQ(chosen_point.y, peak.eval(chosen_point.x).all());
+
+    size_t chosen_var_idx = variable.x_index;
+
+    std::vector<double> x_val;
+    std::vector<double> val_val;
+    std::vector<double> chi_sq_norm;
+    std::vector<double> amp_gradient;
+
+    Eigen::VectorXd chan_gradients;
+    for (double val_x = -4; val_x < 4; val_x += 0.2)
+    {
+      x_val.push_back(val_x);
+      val_val.push_back(variable.val_at(val_x));
+
+      variable.x(val_x);
+      chan_gradients.setConstant(idx, 0.0);
+      double FTotal = peak.eval_grad(chosen_point.x, chan_gradients).all();
+      double t3 = -2.0 * (chosen_point.y - FTotal) / square(chosen_point.weight_phillips_marlow);
+
+      chi_sq_norm.push_back(chan_gradients[chosen_var_idx] * t3);
+      amp_gradient.push_back(
+          square((chosen_point.y - FTotal) / chosen_point.weight_phillips_marlow)
+              / degrees_freedom);
+    }
+
+    MESSAGE() << "chi_sq_norm(x):\n" << visualize(x_val, chi_sq_norm, 100) << "\n";
+    MESSAGE() << "amp_gradient(x):\n" << visualize(x_val, amp_gradient, 100) << "\n";
+    MESSAGE() << "chi_sq_norm(val):\n" << visualize(val_val, chi_sq_norm, 100) << "\n";
+    MESSAGE() << "amp_gradient(val):\n" << visualize(val_val, amp_gradient, 100) << "\n";
+  }
+
+  void grad(DAQuiri::Value& variable)
+  {
+    auto wdata = generate_data();
+
+    int32_t idx {0};
+    peak.update_indices(idx);
+    EXPECT_EQ(idx, 3);
+
+    double degrees_freedom = wdata.data.size() - idx;
+    EXPECT_EQ(degrees_freedom, 17);
+
+    size_t chosen_var_idx = variable.x_index;
+
+    std::vector<double> x_val;
+    std::vector<double> val_val;
+    std::vector<double> chi_sq_norm;
+    std::vector<double> amp_gradient;
+
+    Eigen::VectorXd gradients;
+    Eigen::VectorXd chan_gradients;
+    for (double val_x = -4; val_x < 4; val_x += 0.2)
+    {
+      x_val.push_back(val_x);
+      val_val.push_back(variable.val_at(val_x));
+      variable.x(val_x);
+
+      gradients.setConstant(idx, 0.0);
+      Eigen::VectorXd chan_gradients;
+      double Chisq{0.0};
+      for (const auto& data : wdata.data)
+      {
+        chan_gradients.setConstant(idx, 0.0);
+
+        double FTotal = peak.eval_grad(data.x, chan_gradients).all();
+
+        double t3 = -2.0 * (data.y - FTotal) / square(data.weight_phillips_marlow);
+
+        for (size_t var = 0; var < idx; ++var)
+          gradients[var] += chan_gradients[var] * t3;
+        Chisq += square((data.y - FTotal) / data.weight_phillips_marlow);
+      }
+
+      chi_sq_norm.push_back(gradients[chosen_var_idx]);
+      amp_gradient.push_back(Chisq / degrees_freedom);
+    }
+
+
+    MESSAGE() << "chi_sq_norm(x):\n" << visualize(x_val, chi_sq_norm, 100) << "\n";
+    MESSAGE() << "amp_gradient(x):\n" << visualize(x_val, amp_gradient, 100) << "\n";
+    MESSAGE() << "chi_sq_norm(val):\n" << visualize(val_val, chi_sq_norm, 100) << "\n";
+    MESSAGE() << "amp_gradient(val):\n" << visualize(val_val, amp_gradient, 100) << "\n";
+  }
+
+  DAQuiri::Peak peak;
 };
 
-TEST_F(Step, GamValueTrue)
+TEST_F(Peak, CheckPeakSetup)
 {
-
+  MESSAGE() << "Peak:\n" << peak.to_string(" ") << "\n";
 }
 
+TEST_F(Peak, EvalGaussianOnly)
+{
+  std::vector<double> channels;
+  std::vector<double> y;
+  for (size_t i = 0; i < 20; ++i)
+  {
+    channels.push_back(i);
+    y.push_back(peak.eval(i).all());
+  }
+  MESSAGE() << "peak(channel):\n" << visualize(channels, y, 100) << "\n";
+}
+
+TEST_F(Peak, GradAmplitudeAtOnePoint)
+{
+  grad_at_point(peak.amplitude, 10);
+}
+
+TEST_F(Peak, GradAmplitudeOnly)
+{
+  grad(peak.amplitude);
+}
+
+TEST_F(Peak, GradWidthAtOnePoint)
+{
+  grad_at_point(peak.width_, 5);
+}
+
+TEST_F(Peak, GradWidthOnly)
+{
+  grad(peak.width_);
+}
+
+TEST_F(Peak, GradPositionAtOnePoint)
+{
+  grad_at_point(peak.position, 10);
+}
+
+TEST_F(Peak, GradPositionOnly)
+{
+  grad(peak.position);
+}
--- tests/core/fitting/hypermet/value.cpp
+++ tests/core/fitting/hypermet/value.cpp
@@ -1,12 +1,14 @@
 #include "gtest_color_print.h"
 
+#include "visualize_vector.h"
+
 #include <core/fitting/hypermet/Value.h>
 
-class Value : public TestBase
+class ValueGam : public TestBase
 {
 };
 
-TEST_F(Value, GamValueTrue)
+TEST_F(ValueGam, GamValueTrue)
 {
   DAQuiri::ValueGam vg;
   vg.val(4.0);
@@ -15,10 +17,51 @@ TEST_F(Value, GamValueTrue)
   EXPECT_DOUBLE_EQ(vg.val_at(9.0), 81.0);
 }
 
-TEST_F(Value, GamGradient)
+TEST_F(ValueGam, GamGradient)
 {
   DAQuiri::ValueGam vg;
   vg.val(9.0);
   EXPECT_DOUBLE_EQ(vg.grad(), 6.0);
   EXPECT_DOUBLE_EQ(vg.grad_at(5.0), 10.0);
 }
+
+
+class Value : public TestBase
+{
+};
+
+
+TEST_F(Value, ValBounds)
+{
+  DAQuiri::Value v;
+  v.bound(10, 20);
+
+  v.val(10);
+  EXPECT_EQ(v.val(), 10);
+  v.val(20);
+  EXPECT_EQ(v.val(), 20);
+  v.val(5);
+  EXPECT_EQ(v.val(), 10);
+  v.val(30);
+  EXPECT_EQ(v.val(), 20);
+}
+
+TEST_F(Value, ValAt)
+{
+  DAQuiri::Value val;
+  val.bound(10, 20);
+
+  std::vector<double> x;
+  std::vector<double> y;
+  std::vector<double> grad;
+  for (double v = -7.0; v < 7.0; v+=0.1)
+  {
+    x.push_back(v);
+    y.push_back(val.val_at(v));
+    grad.push_back(val.grad_at(v));
+  }
+
+  MESSAGE() << "Value::val_at(x):\n" << visualize(x, y, 100) << "\n";
+  MESSAGE() << "grad(val):\n" << visualize(y, grad, 100) << "\n";
+}
+
